c4c6c5ee538f72daac0fe33e41cbd46229dbcf79
Gideon Julian <gideonchinonso77@gmail.com>
2025-09-05 14:14:23 +0100

    updated

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import BottomNav from '../components/BottomNav'
-import InstallPrompt from '../components/InstallPrompt'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import BottomNav from "../components/BottomNav";
+import InstallPrompt from "../components/InstallPrompt";
 
 const Layouts = () => {
   return (
     <div>
-        <Outlet />
-        <div>
-             <BottomNav />
-        </div>
-        <div>
-           <InstallPrompt />
-        </div>
+      <Outlet />
+      <div>
+        <BottomNav />
+      </div>
+      <div>
+        <InstallPrompt />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layouts
+export default Layouts;
